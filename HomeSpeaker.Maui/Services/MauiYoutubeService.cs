6ecf2ef1ffdb3e371867e04610900333d9456bb8
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2025-03-14 16:31:23 -0600

    want to implent youtube cacheing

---
+++ HomeSpeaker.Maui/Services/MauiYoutubeService.cs
@@ -0,0 +1,19 @@
+﻿using static HomeSpeaker.Shared.HomeSpeaker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
+using TagLib;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Search;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+using TagFile = TagLib.File;
+
+namespace HomeSpeaker.Maui.Services;
+
+public class MauiYoutubeService
+{
+
+}
